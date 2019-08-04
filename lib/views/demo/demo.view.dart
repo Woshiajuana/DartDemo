c4f4a3ff7ed8228aacfe3dbba19fd137167b4c8e
Woshiajuana <zhigang.chen@owulia.com>
2019-08-04 13:38:54 +0800

    submit about and demo and util

---
+++ lib/views/demo/demo.view.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/material.dart';
+
+class DemoView extends StatefulWidget {
+  @override
+  _DemoViewState createState() => _DemoViewState();
+}
+
+class _DemoViewState extends State<DemoView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/views/util/util.view.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/material.dart';
+
+class UtilView extends StatefulWidget {
+  @override
+  _UtilViewState createState() => _UtilViewState();
+}
+
+class _UtilViewState extends State<UtilView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
