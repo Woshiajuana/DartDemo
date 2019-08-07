dfc16b8f0abe24e82e3fb0dd8bbccfff77ce6075
Woshiajuana <zhigang.chen@owulia.com>
2019-08-07 16:14:46 +0800

    sub app.view.dart and home.view.dart

--- lib/views/home/home.view.dart
+++ lib/views/home/home.view.dart
@@ -11,7 +11,41 @@ class _HomeViewState extends State<HomeView> {
   Widget build(BuildContext context) {
     return Container(
       color: Colors.red,
-      child: new Container(),
+      child: new Stack(
+        children: <Widget>[
+          _widgetMainSection(),
+          _widgetSearchSection(),
+        ],
+      ),
+    );
+  }
+
+  Widget _widgetMainSection () {
+    return new ListView(
+      children: <Widget>[
+        new Container(
+          color: Colors.blue,
+          height: 500.0,
+          margin: const EdgeInsets.only(bottom: 10.0),
+        ),
+        new Container(
+          color: Colors.blue,
+          height: 500.0,
+          margin: const EdgeInsets.only(bottom: 10.0),
+        ),
+      ],
+    );
+  }
+
+  Widget _widgetSearchSection () {
+    return new Positioned(
+      bottom: 0,
+      left: 0,
+      right: 0,
+      child: new Container(
+        height: 100.0,
+        color: Colors.amber,
+      ),
     );
   }
 }
